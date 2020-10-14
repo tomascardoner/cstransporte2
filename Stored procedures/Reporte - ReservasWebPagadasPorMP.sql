6ccd46f24273c407049b1d217f0d5a968c14a9d7
Tomás A. Cardoner <tomas@cardoner.com.ar>
2020-10-14 16:18:05 -0300

    Se agregó el código de pago al listado de reservas web con pago por MP

--- Stored procedures/Reporte - ReservasWebPagadasPorMP.sql
+++ Stored procedures/Reporte - ReservasWebPagadasPorMP.sql
@@ -16,9 +16,12 @@ CREATE PROCEDURE uspObtenerReservasWebPagadasConMP
 		-- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
 		SET NOCOUNT ON;
 
-		SELECT vd.IDViajeDetalle, vd.FechaHora, vd.IDRuta, vd.Orden, dbo.udf_GetEntidadApellidoYNombre(p.Apellido, p.Nombre) AS ApellidoNombre, vd.FacturaNumero
+		SELECT vd.IDViajeDetalle, vd.FechaHora, vd.IDRuta, vd.Orden, dbo.udf_GetEntidadApellidoYNombre(p.Apellido, p.Nombre) AS ApellidoNombre, vd.FacturaNumero, pa.codigoPago AS CodigoPago
 			FROM ViajeDetalle AS vd
                 INNER JOIN Persona AS p ON vd.IDPersona = p.IDPersona
+				INNER JOIN CSTransporte_Web_LobosBus.dbo.Reserva AS r ON vd.IDViajeDetalle = r.idViajeDetalle
+				INNER JOIN CSTransporte_Web_LobosBus.dbo.PagoReserva AS pr ON r.idReserva = pr.idReserva
+				INNER JOIN CSTransporte_Web_LobosBus.dbo.Pago AS pa ON pr.idPago = pa.idPago
 			WHERE vd.FechaHora BETWEEN @FechaHoraDesde AND @FechaHoraHasta
 				AND vd.IDUsuarioCreacion = 151 AND vd.ImporteContado > 0 AND vd.Estado = '1CO' AND vd.OcupanteTipo = 'PA'
 				AND vd.IDMedioPago = 9
@@ -26,4 +29,13 @@ CREATE PROCEDURE uspObtenerReservasWebPagadasConMP
 GO
 
 GRANT EXECUTE ON dbo.uspObtenerReservasWebPagadasConMP TO cstransporte
+GO
+
+USE CSTransporte_Web_LobosBus
+GO
+GRANT SELECT ON CSTransporte_Web_LobosBus.dbo.Reserva TO cstransporte
+GO
+GRANT SELECT ON CSTransporte_Web_LobosBus.dbo.PagoReserva TO cstransporte
+GO
+GRANT SELECT ON CSTransporte_Web_LobosBus.dbo.Pago TO cstransporte
 GO
