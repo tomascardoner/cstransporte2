a2afeb4107ff74f9dfa66ad6a7748db2ee517300
Tomás A. Cardoner <tomas@cardoner.com.ar>
2021-09-05 11:58:56 -0300

    Version Launcher added

---
+++ Version Launcher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSLauncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (args.Length == 0)
+            {
+                MessageBox.Show("Debe especificar el nombre del archivo INI como argumento de línea de comandos.", CardonerSistemas.My.Application.Info.Title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            if (args.Length > 1)
+            {
+                MessageBox.Show("Sólo debe especificar 1 argumento de línea de comandos (nombre del archivo INI sin la extensión).", CardonerSistemas.My.Application.Info.Title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            if (args[0].EndsWith(".ini", StringComparison.InvariantCultureIgnoreCase))
+            {
+                MessageBox.Show("Debe especificar el nombre del archivo INI sin la extensión.", CardonerSistemas.My.Application.Info.Title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            string iniFileName = args[0];
+            string iniFileFullName = args[0] + ".ini";
+            string iniFileFullPath = System.IO.Path.Combine(Application.StartupPath, iniFileFullName);
+
+            CardonerSistemas.Configuration.IniFile.IniFile iniFile = new CardonerSistemas.Configuration.IniFile.IniFile();
+            iniFile.Read(iniFileFullPath, false);
+            CardonerSistemas.Configuration.IniFile.Key key = iniFile.GetKey("ExeFileName", "System");
+            string exeFileName;
+            if (key == null)
+            {
+                return;
+            }
+            exeFileName = key.Value;
+
+            string exeFileFullPath = System.IO.Path.Combine(Application.StartupPath, exeFileName);
+            if (System.IO.File.Exists(exeFileFullPath))
+            {
+                System.Diagnostics.Process.Start(exeFileFullPath, $"CONFIG={iniFileName}");
+            }
+        }
+    }
+}
---
+++ Version Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CS-Launcher 2021")]
+[assembly: AssemblyDescription("Application launcher")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cardoner Sistemas")]
+[assembly: AssemblyProduct("CS-Launcher")]
+[assembly: AssemblyCopyright("Copyright © 2021 Cardoner Sistemas")]
+[assembly: AssemblyTrademark("® CS-Launcher 2021")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c96b1da8-3d4f-4a1b-8212-9335c2f205c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
